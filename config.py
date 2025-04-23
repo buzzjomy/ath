2768e05757a5504d7ff3cb68feee12bb0ced2d63
buzzjomy <42223415+buzzjomy@users.noreply.github.com>
2025-04-23 12:10:23 +0000

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+firelight_bucket_name = 'intake-api-firelight-s3-713881800640'
+input_file_path = f'C:\\Users\\E406692\\Downloads\\4_15_2025\\4_15_2025'
+orig_file_names = [f'TA1.xml',f'TA1_Single.pdf']
+region = 'us-east-1'
+firelight_lambda_func = 'intake-api-firelight-function'
+hyperscience_bucket_name = 'intake-api-hyperscience-s3-713881800640' 
+expected_flight_json_file = f'C:\\Workitems\\code\\Story\\1844\\expected_flight_output.json'
--- test.py
+++ test.py
@@ -1,63 +1,77 @@
-# Automate the loading of input files to S3
+from deepdiff import DeepDiff
 import random, string
 import boto3
 import os
 import shutil
 import json
 from test_event_json import json_str
+import config
 
 # Create a session
-s3 = boto3.client('s3', 'us-east-1')
+s3 = boto3.client('s3', config.region)
 
+# Automate the loading of input files to S3
 # Specify bucket and file information
-bucket_name = 'intake-api-firelight-s3-713881800640'
+firelight_bucket_name = config.firelight_bucket_name
 
 suffix = ''.join(random.choices(string.ascii_letters + string.digits, k=5))
-file_path = f'C:\\Users\\E406692\\Downloads\\4_15_2025\\4_15_2025'
-file_names = [f'TA1_{suffix}.xml',
+input_file_path = config.input_file_path
+test_file_names = [f'TA1_{suffix}.xml',
               f'TA1_{suffix}_Single.pdf']
-orig_file_names = [f'TA1.xml',f'TA1_Single.pdf']
+orig_file_names = config.orig_file_names
 
 for i in range(2):
-    shutil.copyfile(os.path.join(file_path, orig_file_names[i]), 
-                    os.path.join(file_path, file_names[i]))
+    shutil.copyfile(os.path.join(input_file_path, orig_file_names[i]), 
+                    os.path.join(input_file_path, test_file_names[i]))
 
 object_name = 'input_path/'  # Optional; if not specified, file_name is used
 
 # Upload the file
-body = '{\"Records\":[{\"eventVersion\":\"2.1\",\"eventSource\":\"aws:s3\",\"awsRegion\":\"us-east-1\",\"eventTime\":\"2025-02-07T10:53:14.326Z\",\"eventName\":\"ObjectCreated:Put\",\"userIdentity\":{\"principalId\":\"AWS:AROA2MNVLZ7AP74VVIX7I:intake-api-s3-presigned-function\"},\"requestParameters\":{\"sourceIPAddress\":\"4.188.244.164\"},\"responseElements\":{\"x-amz-request-id\":\"0FEHYH94A3RCFQR0\",\"x-amz-id-2\":\"zl0mBoxKuGxeMI8sIYli1royiFw6YLwjjY/rSrePKI+fqiIFXjnf9gOssrzF6CUX7rKR+JtLFlUcyaFrHFLU0zb4Y60w7TjneEBR+0mzVmM=\"},\"s3\":{\"s3SchemaVersion\":\"1.0\",\"configurationId\":\"NzNhZjYyMjgtM2Y3NS00MGI2LWE0ZGMtMWFlNWNmYTNkNjUw\",\"bucket\":{\"name\":\"intake-api-firelight-s3-713881800640\",\"ownerIdentity\":{\"principalId\":\"A2QRQVI3CQ4H3L\"},\"arn\":\"arn:aws:s3:::intake-api-firelight-s3-713881800640\"},\"object\":{\"key\":\"input_path/"+ file_names[0] + "\",\"size\":4681479,\"eTag\":\"98a1903142b9e775eea900f648e0f54e\",\"versionId\":\"SA.JmpR.9X6w_.NxC1VnKaYEkpn07mB1\",\"sequencer\":\"0067A5E61A287AAC70\"}}}]}'.replace('"','\\"')
-input(body)
 try:
-    for file_name in file_names:
-        output = s3.upload_file(os.path.join(file_path, file_name), bucket_name, object_name + file_name)
-        print(output)
-        print(f"File {file_name} uploaded to {bucket_name}/{object_name + file_name}")
+    for file_name in test_file_names:
+        output = s3.upload_file(os.path.join(input_file_path, file_name), firelight_bucket_name, object_name + file_name)
+        print(f"File {file_name} uploaded to {firelight_bucket_name}/{object_name + file_name}")
 except Exception as ex:
     print(ex)
 
 
 # Trigger the function to run intake api
-# f = open('test_event.json',)
-# test_event_json = json.load(f)
+test_event_json_str = json_str.replace("TTTT.xml", test_file_names[0])
 
-# test_event_json['Records'][0]['body']=test_event_json['Records'][0]['body']\
-#     .replace('"', '\\"').replace("^^TEST_FL_XML^^", 
-#              file_names[0])
-# test_event_json_str = str(test_event_json).replace('\'', '"')
-# input(test_event_json_str)
-test_event_json_str = json_str.replace("TTTT.xml", file_names[0])
-input(test_event_json_str)
-
-lambda_client = boto3.client('lambda', 'us-east-1')
+lambda_client = boto3.client('lambda', config.region)
 
 response = lambda_client.invoke(
-    FunctionName='intake-api-firelight-function',
+    FunctionName=config.firelight_lambda_func,
     InvocationType='RequestResponse',  # Or 'Event' for asynchronous invocation
     Payload=str(test_event_json_str).encode('utf-8')  # JSON payload as bytes
 )
 
 print(response['StatusCode'])
+print(response['ResponseMetadata']['RequestId'])
 
 # Go to S3 to get the flight_output.json
-# Get the transaction id and then search for the state machine execution which has transaction
+hyperscience_bucket_name = config.hyperscience_bucket_name
+object_key = f'test14/{response['ResponseMetadata']['RequestId']}/flight_output.json'   
 
+try:
+    response = s3.get_object(Bucket=hyperscience_bucket_name, Key=object_key)
+    content = response['Body'].read()
+    actual_content = content.decode('utf-8') 
+    actual_content = json.loads(actual_content)
+    with open("flight_output.json", "w") as f:
+        f.write(str(actual_content))
+except Exception as e:
+    print(f"Error retrieving object: {e}")
+
+#Compare the flight output with the expected output
+with open(config.expected_flight_json_file, "r") as f:
+    expected_content = json.load(f)
+dd_out = DeepDiff(actual_content, 
+                expected_content, 
+                ignore_order=False, 
+                exclude_paths=["root['transaction']['transaction_id']"],
+                exclude_regex_paths=[r"root\['annuitant'\]\['address'\]\[\d+\]\['item_id'\]"])
+
+print(dd_out)
+# Get the transaction id from flight json and 
+# then search for the state machine execution which has transaction id
